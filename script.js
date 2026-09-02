cf06e2731a0ea526c72e6fc9d3f53e9c655b3255
shruthi-07-goud <24eg107e41@anurag.edu.in>
2026-09-02 04:55:29 -0400

    Update and rename catalogue.js to script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+let count = 0;
+
+function addToCart(item) {
+    count++;
+
+    document.getElementById("cart").innerHTML =
+        "Cart: " + count + " items";
+
+    alert(item + " added to cart!");
+}
+
+function searchItems() {
+
+    let search = document.getElementById("search")
+                    .value.toLowerCase();
+
+    let items = document.getElementsByClassName("item");
+
+    for (let i = 0; i < items.length; i++) {
+
+        let name = items[i]
+                    .getElementsByTagName("h3")[0]
+                    .innerText.toLowerCase();
+
+        if (name.includes(search)) {
+            items[i].style.display = "block";
+        }
+        else {
+            items[i].style.display = "none";
+        }
+    }
+}
